2b2920097cfc856a797194fceb4d583d89760351
calangheiyo <393196978@qq.com>
2018-03-08 17:42:28 +0800

    Compile Success
    Compile Success

--- solr-login-site/src/main/java/org/calangheiyo/solrlogin/web/controller/HoneyController.java
+++ solr-login-site/src/main/java/org/calangheiyo/solrlogin/web/controller/HoneyController.java
@@ -8,14 +8,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Created by sukaixiang on 2017/9/29.
  */
 @Controller
-public class HomeController {
-    @RequestMapping(value = {"/","/home"}, method = RequestMethod.GET)
-    public String home(){
-        return "home/home";
-    }
+public class HoneyController {
 
-    @RequestMapping(value = "/app", method = RequestMethod.GET)
-    public String app(){
-        return "home/home";
+    @RequestMapping(value = "/honey", method = RequestMethod.GET)
+    public String honey(){
+        return "honey/honey";
     }
 }
