e3aaf4e32748c8bab01c37f8a6c39a3867f7df44
calangheiyo <393196978@qq.com>
2018-03-09 15:18:38 +0800

    Build tocken module
    Build tocken module

--- solr-login-site/src/main/java/org/calangheiyo/solrlogin/web/controller/HoneyController.java
+++ solr-login-site/src/main/java/org/calangheiyo/solrlogin/web/controller/HoneyController.java
@@ -1,4 +1,4 @@
-package org.calangheiyo.honey.controller;
+package org.calangheiyo.solrlogin.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
